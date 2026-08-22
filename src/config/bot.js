05bad890d34260b20d1bf6eb4f078f8a2f0366b3
kochisakuraba-ux <kochisakuraba@gmail.com>
2026-08-22 16:26:54 +0000

    commit

--- src/config/bot.js
+++ src/config/bot.js
@@ -109,7 +109,7 @@ export const botConfig = {
 
       // Discord-style palette shortcuts.
       blurple: "#5865F2",
-      green: "#57F287",
+      green: "#4878fc",
       yellow: "#FEE75C",
       fuchsia: "#EB459E",
       red: "#ED4245",
